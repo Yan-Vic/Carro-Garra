12bdadf2570ac74a6986957b1d965344338c49e1
Yan-Vic <yan.vic98@gmail.com>
2025-04-25 21:20:56 -0300

    Third Commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+#include <ESP32Servo.h>
+#include <vector>
+
+const int NUM_KEYS = 3;
+const int buttonPins[NUM_KEYS] = {5, 18, 19};
+const int BUZZER_PIN = 23;
+const int BUZZER_CHANNEL = 0;
+const int Servo1 = 25;
+const int Servo2 = 26;
+const int Garra = 27;
+
+#define Bt_Iniciar 32;
 
 // put function declarations here:
 int myFunction(int, int);
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
